c0af70e29160983b848b3be16b86844ed1122b89
Dorian Marié <dorian.marie.france@gmail.com>
2020-04-07 13:14:46 +0200

    prepare for more test cases

--- test/Template.test.js
+++ test/Template.test.js
@@ -1,42 +1,62 @@
 import { expect } from "chai"
 import Template from "../src/Template"
 
+let data = {
+  name: "Dorian",
+  sleeping: false,
+  books: [
+    {
+      title: "Hackers and Painters",
+      author: "Paul Graham"
+    }
+  ]
+}
+
+let filters = {
+  upcase: function(value, arguments = {}) {
+    value.toUpperCase()
+  },
+  downcase: function(value, arguments = {}) {
+    value.toLowerCase()
+  }
+}
+
 describe("Template", function() {
   it("works on an empty string", function() {
-    expect(Template.render("", {})).to.equal("")
+    expect(Template.render("", data)).to.equal("")
   })
 
   it("works on a simple string", function() {
-    expect(Template.render("hello", {})).to.equal("hello")
+    expect(Template.render("hello", data)).to.equal("hello")
   })
 
   it("works on a simple variable interpolation", function() {
-    expect(Template.render("hello {{ name }}", { "name": "Dorian" })).to.equal(
+    expect(Template.render("hello {{ name }}", data)).to.equal(
       "hello Dorian"
     )
   })
 
   it("works on a number interpolation", function() {
-    expect(Template.render("hello {{ 2 }}", {})).to.equal(
+    expect(Template.render("hello {{ 2 }}", data)).to.equal(
       "hello 2"
     )
   })
 
   it("works on a boolean interpolation", function() {
-    expect(Template.render("{{ true }} / {{ false }}", {})).to.equal(
+    expect(Template.render("{{ true }} / {{ false }}", data)).to.equal(
       "true / false"
     )
   })
 
   it("works on an array interpolation", function() {
-    expect(Template.render("{{ 1, true, name }}", { "name": "Dorian" })).to.equal(
+    expect(Template.render("{{ 1, true, name }}", data)).to.equal(
       "[1, true, Dorian]"
     )
   })
 
   it("works on an hash interpolation", function() {
     expect(Template.render(
-      "{{ name: name, numbers: [1, 2, 3] }}", { "name": "Dorian" }
+      "{{ name: name, numbers: [1, 2, 3] }}", data
     )).to.equal(
       "{ name: Dorian, numbers: [1, 2, 3] }"
     )
@@ -44,7 +64,7 @@ describe("Template", function() {
 
   it("works on an nil, float, string interpolation", function() {
     expect(Template.render(
-      "{{ null, 1.2, 'hello' }}", {}
+      "{{ null, 1.2, 'hello' }}", data
     )).to.equal(
       "[null, 1.2, hello]"
     )
